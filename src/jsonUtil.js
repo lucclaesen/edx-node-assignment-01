250f4f37c62cc94f18b1b3da8268e9ab5e77d66f
lucclaesen <luc.claesen@gmail.com>
2017-12-08 18:00:46 +0100

    Corrected documentation

--- src/jsonUtil.js
+++ src/jsonUtil.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
+const path = require("path");
 
 /**
  * Saves the given objects as json to a file with the given path.
  * @param {string}          The file path. 
- * @param {Promise<void>}   A promise for the completion of the save operation. 
+ * @param {[any]}           The array of objects to save as json
+ * @returns {Promise<void>}   A promise for the completion of the save operation. 
  */
 function saveObj2File(filePath, obj) {
     return new Promise((resolve, reject) => {
